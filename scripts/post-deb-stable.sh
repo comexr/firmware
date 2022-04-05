dd1d539a6551cf3fc3dce244904158f1b1fdc0ec
Laurens Bruins <l.bruins@laptopwithlinux.com>
2022-04-05 11:28:16 +0200

    added tuxedo-keyboard to stable branch

--- scripts/post-deb-stable.sh
+++ scripts/post-deb-stable.sh
@@ -14,14 +14,14 @@ package_loc=$(git rev-parse --show-toplevel)/repository/dists/stable/main/binary
 root_loc=$(git rev-parse --show-toplevel)
 rm "$package_loc"/*
 cd $root_loc/repository
-dpkg-scanpackages -m pool/main > "$package_loc"/Packages
+dpkg-scanpackages -m pool/stable > "$package_loc"/Packages
 cat "$package_loc"/Packages | gzip -9 > "$package_loc"/Packages.gz
 
 #Release files
 release_loc=$(git rev-parse --show-toplevel)/repository/dists/stable
 sign="Laurens Bruins"
 cd "$release_loc"
-$root_loc/scripts/generate-release.sh > Release
+$root_loc/scripts/generate-release-stable.sh > Release
 cat $root_loc/KEY.gpg | gpg --import
 cat "$release_loc"/Release | gpg --default-key "$sign" -abs > "$release_loc"/Release.gpg
 cat "$release_loc"/Release | gpg --default-key "$sign" -abs --clearsign > "$release_loc"/InRelease

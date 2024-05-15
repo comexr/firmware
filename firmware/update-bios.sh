f522c05980b7e106b16abdb399e24900c0621719
COMEXR B.V <64539804+comexr@users.noreply.github.com>
2024-05-15 17:36:04 +0200

    Add bios updating script + firmware files

---
+++ firmware/update-bios.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Determine model number
+model=$(sudo dmidecode -s system-version)
+
+#Download flashrom binary
+wget https://raw.githubusercontent.com/comexr/firmware/main/firmware/flashrom -O /tmp/flashrom
+
+#Get firmware for this model
+wget https://raw.githubusercontent.com/comexr/firmware/main/firmware/$model/firmware.rom -O /tmp/firmware.rom
+wget https://raw.githubusercontent.com/comexr/firmware/main/firmware/$model/ec.rom -O /tmp/ec.rom
+
+#Flash firmware
+sudo /tmp/flashrom -p internal -w /tmp/firmware.rom
+sudo /tmp/flashrom -p ite_ec:boardmismatch=force -w /tmp/ec.rom
+
+#Reboot to apply
+sudo reboot

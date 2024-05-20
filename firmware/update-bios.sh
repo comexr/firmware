f5c8f6530da8e3f4056a00963a6c81b3fb39d548
COMEXR B.V <64539804+comexr@users.noreply.github.com>
2024-05-20 13:54:03 +0200

    Update flashing script

--- firmware/update-bios.sh
+++ firmware/update-bios.sh
@@ -8,15 +8,19 @@ wget https://raw.githubusercontent.com/comexr/firmware/main/firmware/flashrom -O
 chmod +x /tmp/flashrom
 
 #Download boot.efi
-
+wget https://raw.githubusercontent.com/comexr/firmware/main/firmware/boot.efi /tmp/boot.efi
 
 #Get firmware for this model
 wget https://raw.githubusercontent.com/comexr/firmware/main/firmware/$model/firmware.rom -O /tmp/firmware.rom
 wget https://raw.githubusercontent.com/comexr/firmware/main/firmware/$model/ec.rom -O /tmp/ec.rom
 
+#Create and copy to folder
+sudo mkdir -p /boot/efi/firmware-update/firmware
+sudo cp /tmp/boot.efi /tmp/firmware.rom /tmp/ec.rom /boot/efi/firmware-update/firmware
+
 #Flash firmware
-sudo /tmp/flashrom -p internal -w /tmp/firmware.rom
-sudo /tmp/flashrom -p ite_ec:boardmismatch=force -w /tmp/ec.rom
+#sudo /tmp/flashrom -p internal -w /tmp/firmware.rom
+#sudo /tmp/flashrom -p ite_ec:boardmismatch=force -w /tmp/ec.rom
 
 #Reboot to apply
 #sudo reboot

cdbface26d480144c4808ad450ad30426958ebb8
COMEXR B.V <64539804+comexr@users.noreply.github.com>
2024-05-20 15:38:30 +0200

    Update flashing script

--- firmware/update-bios.sh
+++ firmware/update-bios.sh
@@ -3,24 +3,32 @@
 #Determine model number
 model=$(sudo dmidecode -s system-version)
 
-#Download flashrom binary
-wget https://raw.githubusercontent.com/comexr/firmware/main/firmware/flashrom -O /tmp/flashrom
-chmod +x /tmp/flashrom
-
 #Download boot.efi
 wget https://raw.githubusercontent.com/comexr/firmware/main/firmware/boot.efi -O /tmp/boot.efi
 
-#Get firmware for this model
+#Get EC firmware
 wget https://raw.githubusercontent.com/comexr/firmware/main/firmware/$model/firmware.rom -O /tmp/firmware.rom
+
+#Move firmware files
+sudo mkdir -p /boot/efi/firmware-update/firmware
+sudo cp /tmp/boot.efi /boot/efi/firmware-update
+sudo cp /tmp/ec.rom /boot/efi/firmware-update/firmware
+
+#Flash EC
+sudo efibootmgr --quiet --create --bootnum 1000 --disk /dev/nvme0n1 --part 1 --loader "\firmware-update\boot.efi" --label firmware-update
+sudo shutdown 0
+
+# After reboot:
+
+#Determine model number
+model=$(sudo dmidecode -s system-version)
+
+#Download flashrom binary (+ make executable)
+wget https://raw.githubusercontent.com/comexr/firmware/main/firmware/flashrom -O /tmp/flashrom
+chmod +x /tmp/flashrom
+
+#Get BIOS firmware
 wget https://raw.githubusercontent.com/comexr/firmware/main/firmware/$model/ec.rom -O /tmp/ec.rom
 
-#Create and copy to folder
-sudo mkdir -p /boot/efi/firmware-update/firmware
-sudo cp /tmp/boot.efi /tmp/firmware.rom /tmp/ec.rom /boot/efi/firmware-update/firmware
-
-#Flash firmware
-#sudo /tmp/flashrom -p internal -w /tmp/firmware.rom
-#sudo /tmp/flashrom -p ite_ec:boardmismatch=force -w /tmp/ec.rom
-
-#Reboot to apply
-#sudo reboot
+#Flash BIOS
+sudo /tmp/flashrom -p internal -w /tmp/firmware.rom
